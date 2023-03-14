05e350a8b7849ae2531b4b813c851b0bd94ef83a
Andres <andresvazquezo1@hotmail.com>
2023-03-14 16:37:12 -0600

    Add project files.

---
+++ Unidad5_Arreglos/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Unidad5_Arreglos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNota11 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtNota13 = new System.Windows.Forms.TextBox();
+            this.txtNota12 = new System.Windows.Forms.TextBox();
+            this.txtNota23 = new System.Windows.Forms.TextBox();
+            this.txtNota22 = new System.Windows.Forms.TextBox();
+            this.txtNota21 = new System.Windows.Forms.TextBox();
+            this.txtNota33 = new System.Windows.Forms.TextBox();
+            this.txtNota32 = new System.Windows.Forms.TextBox();
+            this.txtNota31 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtNota41 = new System.Windows.Forms.TextBox();
+            this.txtNota42 = new System.Windows.Forms.TextBox();
+            this.txtNota43 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(360, 208);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(102, 46);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNota11
+            // 
+            this.txtNota11.Location = new System.Drawing.Point(42, 50);
+            this.txtNota11.Name = "txtNota11";
+            this.txtNota11.Size = new System.Drawing.Size(100, 22);
+            this.txtNota11.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 271);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 167);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // txtNota13
+            // 
+            this.txtNota13.Location = new System.Drawing.Point(42, 173);
+            this.txtNota13.Name = "txtNota13";
+            this.txtNota13.Size = new System.Drawing.Size(100, 22);
+            this.txtNota13.TabIndex = 3;
+            // 
+            // txtNota12
+            // 
+            this.txtNota12.Location = new System.Drawing.Point(42, 118);
+            this.txtNota12.Name = "txtNota12";
+            this.txtNota12.Size = new System.Drawing.Size(100, 22);
+            this.txtNota12.TabIndex = 4;
+            // 
+            // txtNota23
+            // 
+            this.txtNota23.Location = new System.Drawing.Point(255, 173);
+            this.txtNota23.Name = "txtNota23";
+            this.txtNota23.Size = new System.Drawing.Size(100, 22);
+            this.txtNota23.TabIndex = 5;
+            // 
+            // txtNota22
+            // 
+            this.txtNota22.Location = new System.Drawing.Point(255, 113);
+            this.txtNota22.Name = "txtNota22";
+            this.txtNota22.Size = new System.Drawing.Size(100, 22);
+            this.txtNota22.TabIndex = 6;
+            // 
+            // txtNota21
+            // 
+            this.txtNota21.Location = new System.Drawing.Point(255, 50);
+            this.txtNota21.Name = "txtNota21";
+            this.txtNota21.Size = new System.Drawing.Size(100, 22);
+            this.txtNota21.TabIndex = 7;
+            // 
+            // txtNota33
+            // 
+            this.txtNota33.Location = new System.Drawing.Point(482, 173);
+            this.txtNota33.Name = "txtNota33";
+            this.txtNota33.Size = new System.Drawing.Size(100, 22);
+            this.txtNota33.TabIndex = 8;
+            // 
+            // txtNota32
+            // 
+            this.txtNota32.Location = new System.Drawing.Point(482, 113);
+            this.txtNota32.Name = "txtNota32";
+            this.txtNota32.Size = new System.Drawing.Size(100, 22);
+            this.txtNota32.TabIndex = 9;
+            // 
+            // txtNota31
+            // 
+            this.txtNota31.Location = new System.Drawing.Point(482, 50);
+            this.txtNota31.Name = "txtNota31";
+            this.txtNota31.Size = new System.Drawing.Size(100, 22);
+            this.txtNota31.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Estudiante 1 - Español";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Estudiante 1 - Matematicas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Estudiante 1 - Historia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Estudiante 2 - Español";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 94);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(167, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Estudiante 2 - Matematicas";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(233, 154);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(136, 16);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Estudiante 2 - Historia";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(456, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(140, 16);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Estudiante 3 - Español";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(456, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(167, 16);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Estudiante 3 - Matematicas";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(456, 154);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(136, 16);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Estudiante 3 - Historia";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(639, 94);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(140, 16);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Estudiante 4 - Español";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(639, 154);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(140, 16);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Estudiante 4 - Español";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(639, 31);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(140, 16);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Estudiante 4 - Español";
+            // 
+            // txtNota41
+            // 
+            this.txtNota41.Location = new System.Drawing.Point(658, 50);
+            this.txtNota41.Name = "txtNota41";
+            this.txtNota41.Size = new System.Drawing.Size(100, 22);
+            this.txtNota41.TabIndex = 23;
+            // 
+            // txtNota42
+            // 
+            this.txtNota42.Location = new System.Drawing.Point(658, 113);
+            this.txtNota42.Name = "txtNota42";
+            this.txtNota42.Size = new System.Drawing.Size(100, 22);
+            this.txtNota42.TabIndex = 24;
+            // 
+            // txtNota43
+            // 
+            this.txtNota43.Location = new System.Drawing.Point(658, 173);
+            this.txtNota43.Name = "txtNota43";
+            this.txtNota43.Size = new System.Drawing.Size(100, 22);
+            this.txtNota43.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNota43);
+            this.Controls.Add(this.txtNota42);
+            this.Controls.Add(this.txtNota41);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNota31);
+            this.Controls.Add(this.txtNota32);
+            this.Controls.Add(this.txtNota33);
+            this.Controls.Add(this.txtNota21);
+            this.Controls.Add(this.txtNota22);
+            this.Controls.Add(this.txtNota23);
+            this.Controls.Add(this.txtNota12);
+            this.Controls.Add(this.txtNota13);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtNota11);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNota11;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtNota13;
+        private System.Windows.Forms.TextBox txtNota12;
+        private System.Windows.Forms.TextBox txtNota23;
+        private System.Windows.Forms.TextBox txtNota22;
+        private System.Windows.Forms.TextBox txtNota21;
+        private System.Windows.Forms.TextBox txtNota33;
+        private System.Windows.Forms.TextBox txtNota32;
+        private System.Windows.Forms.TextBox txtNota31;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtNota41;
+        private System.Windows.Forms.TextBox txtNota42;
+        private System.Windows.Forms.TextBox txtNota43;
+    }
+}
+
---
+++ Unidad5_Arreglos/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unidad5_Arreglos
+{
+    public partial class Form1 : Form
+    {
+        private int[,] notas;
+        private int[] promedios;
+        private int promedioGeneral;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            notas = new int[4, 3];
+            notas[0, 0] = int.Parse(txtNota11.Text);
+            notas[0, 1] = int.Parse(txtNota12.Text);
+            notas[0, 2] = int.Parse(txtNota13.Text);
+            notas[1, 0] = int.Parse(txtNota21.Text);
+            notas[1, 1] = int.Parse(txtNota22.Text);
+            notas[1, 2] = int.Parse(txtNota23.Text);
+            notas[2, 0] = int.Parse(txtNota31.Text);
+            notas[2, 1] = int.Parse(txtNota32.Text);
+            notas[2, 2] = int.Parse(txtNota33.Text);
+            notas[3, 0] = int.Parse(txtNota41.Text);
+            notas[3, 1] = int.Parse(txtNota42.Text);
+            notas[3, 2] = int.Parse(txtNota43.Text);
+
+            // Calcular promedio de cada estudiante
+            promedios = new int[4];
+            promedioGeneral = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                int suma = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    suma += notas[i, j];
+                }
+                promedios[i] = suma / 3;
+                promedioGeneral += promedios[i];
+            }
+            promedioGeneral /= 4;
+
+            // Mostrar tabla con resultados
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("Estudiante", "Estudiante");
+            dataGridView1.Columns.Add("Materia 1", "Materia 1");
+            dataGridView1.Columns.Add("Materia 2", "Materia 2");
+            dataGridView1.Columns.Add("Materia 3", "Materia 3");
+            dataGridView1.Columns.Add("Promedio", "Promedio");
+            for (int i = 0; i < 4; i++)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = "Estudiante " + (i + 1);
+                dataGridView1.Rows[i].Cells[1].Value = notas[i, 0];
+                dataGridView1.Rows[i].Cells[2].Value = notas[i, 1];
+                dataGridView1.Rows[i].Cells[3].Value = notas[i, 2];
+                dataGridView1.Rows[i].Cells[4].Value = promedios[i];
+            }
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[4].Cells[0].Value = "Promedio General";
+            dataGridView1.Rows[4].Cells[4].Value = promedioGeneral;
+        }
+    }
+}
